6d633863b3f8daedf5dc2247bdc5ff0b5d0e917b
youbyoub48 <ayou8@hotmail.fr>
2021-05-14 14:49:14 +0200

    aplha

--- scraping.py
+++ scraping.py
@@ -1,23 +1,26 @@
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
 from time import sleep
 from bs4 import BeautifulSoup
 import json
 class Scraping():
-    def __init__(self,browser):
-        horaire = self._Web_Scrap(browser)
+    def __init__(self):
+        horaire = self._Web_Scrap()
         self._Convertisseur(horaire)
-    def _Web_Scrap(self,browser):
-        # Initiate the browser
-        #browser  = webdriver.Chrome(ChromeDriverManager().install())
-        #browser.maximize_window()
-        #browser.get("https://mawaqit.net/fr/m/imam-malik-sorgues")
-        #sleep(3)
+    def _Web_Scrap(self):
+        chromeOptions = Options()
+        chromeOptions.headless = True
+        browser  = webdriver.Chrome("/usr/lib/chromium-browser/chromedriver", options=chromeOptions)
+        browser.get("https://mawaqit.net/fr/m/imam-malik-sorgues")
+        sleep(2)
         browser.find_element_by_id("cookie-accept-button").click()
         soup = BeautifulSoup(browser.page_source, "html.parser")
         horaire = []
         for p in soup.find_all(class_="time"):
             horaire.append(str(p))
+            browser.quit()
         return horaire
     def _Convertisseur(self,html):
         liste =[]
@@ -26,7 +29,7 @@ class Scraping():
             x = line.find('">')
             x += 2
             y = line.find("</")  
-            liste.append(line[x:y])
+            liste.append(line[x:y].replace("<div>",""))
         with open("horaire.json","w") as f:
             json.dump(liste,f,indent=4)          
 
