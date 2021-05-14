57fd906524e7ef42bc3fe1602a2b1d9d17f46841
youbyoub48 <ayou8@hotmail.fr>
2021-05-14 14:46:59 +0200

    aplha

--- main.py
+++ main.py
@@ -5,13 +5,18 @@ import pygame.mixer
 from scraping import Scraping
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
-# Initiate the browser
-browser  = webdriver.Chrome("/usr/lib/chromium-browser/chromedriver")
-browser.maximize_window()
-browser.get("https://mawaqit.net/fr/m/imam-malik-sorgues")
-time.sleep(3)
-os.system("xdotool key F11")
-Scraping(browser)
+def Browser():
+    global browser
+    # Initiate the browser
+    browser  = webdriver.Chrome("/usr/lib/chromium-browser/chromedriver")
+    browser.maximize_window()
+    browser.get("https://mawaqit.net/fr/m/imam-malik-sorgues")
+    time.sleep(3)
+    browser.find_element_by_id("cookie-accept-button").click()
+    os.system("xdotool key F11")
+    browser.execute_script("document.body.style.zoom='80%'")
+Scraping()
+Browser()
 pygame.mixer.init()
 athan = pygame.mixer.Sound("athan.wav")
 s = "%H:%M"
@@ -22,6 +27,8 @@ while True:
     if horaire[0] == a or horaire[1] == a or horaire[2] == a or horaire[3] == a or horaire[4] == a:
         pygame.mixer.Sound.play(athan)
     if a == "00:10":
-        Scraping(browser)
+        Scraping()
+        browser.quit()
+        Browser()
         with open("horaire.json","r") as f:
             horaire = json.load(f)
